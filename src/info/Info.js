15f4a9c6cd2e3f6e804f38ec04e4ea6a990c09f4
noah berlove <noah.berlove@gmail.com>
2022-11-13 15:36:55 -0800

    initial commit

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,140 +1,132 @@
 import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import colorgradient from "../img/colorgradient.jpg"
+import namebadge from "../img/namebadge.jpg"
+import mariopest from "../img/mariopest.jpg"
+import memegenerator from "../img/memegenerator.jpg"
+import traveljournal from "../img/traveljournal.jpg"
+import ajsbodywork from "../img/ajsbodywork.jpg"
+import zengarden from "../img/zengarden.jpg"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
+export let colors = ["rgb(247, 189, 255)", "rgb(82, 174, 255)"];
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Noah",
+    lastName: "Berlove",
+    initials: "NB",
     position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self, 
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
-        {
-            emoji: '☕',
-            text: 'fueled by coffee'
-        },
+    miniBio: [ 
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'Based in the US'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Full Stack Developer"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "Noah.Berlove@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
+            link: "https://facebook.com/noahberlove",
             icon: 'fa fa-facebook',
             label: 'facebook'
         },
+        // {
+        //     link: "https://instagram.com",
+        //     icon: 'fa fa-instagram',
+        //     label: 'instagram'
+        // },
         {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/switch86",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://linkedin.com/in/berloven",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
         {
-            link: "https://twitter.com",
+            link: "https://twitter.com/_switch86",
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Noah. I have a natural aptitude for puzzling - quickly understanding the entirety of a complex challenge, seeing the functions of each component and figuring out how they can efficiently work together to make up the whole. This intuitive skill combined with my passion for accessibility and inclusion are ultimately what drive me toward web development. I am excited to create functional and user-friendly technologies which are equitable, accessible, and improve the lives of a diverse audience of users.",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['javascript', 'react', 'git', 'github', 'node.js', 'html5', 'css3', 'microsoft office'],
+            exposedTo: ['node.js', 'python', 'docker', 'pygame']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'botany',
+            emoji: '🌿'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'gamified learning',
+            emoji: '🎮'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'camping',
+            emoji: '🏕️'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'rock climbing',
+            emoji: '🧗🏻'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [
+    {
+        title: "AJS BodyWork",
+        live: "https://ajsbodywork.surge.sh/", 
+        source: "https://github.com/switch86/assignments/tree/main/ajsBodywork",
+        image: ajsbodywork
+    },
+    {
+        title: "Color Gradient CSS",
+        live: "https://gradientreactappnkb.surge.sh/",
+        source: "https://github.com/switch86/gradientGenerator", 
+        image: colorgradient
+    },
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
+            title: "Name Badge Creator",
+            live: "https://namebadgegeneratornb.surge.sh",
+            source: "https://github.com/switch86/nameBadge",
+            image: namebadge
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock2
+            title: "Mario Coin Counter",
+            live: "https://marionkb.surge.sh/",
+            source: "https://github.com/switch86/assignments/marioPestControl",
+            image: mariopest
         },
         {
-            title: "Project 3",
+            title: "Meme Generator",
             live: "https://paytonpierce.dev",
             source: "https://github.com/paytonjewell",
-            image: mock3
+            image: memegenerator
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
+            title: "Travel Journal",
+            live: "https://traveljournalnkb.surge.sh/",
+            source: "https://github.com/switch86/assignments/travelJournal",
+            image: traveljournal
         },
         {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
+            title: "Zen Garden",
+            live: "https://zengardennkb.surge.sh",
+            source: "https://github.com/switch86/assignments/zenGarden",
+            image: zengarden
         }
     ]
 }
